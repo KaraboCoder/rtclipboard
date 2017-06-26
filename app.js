60950840e6fc570d388cad5d2407b3ba4576b49d
Karabo NGWATO <kngwato@e4r22p1.jnb.42.fr>
2017-06-26 11:33:47 +0200

    added child of uid

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ angular.module('realtime_clipboard')
             Auth.$signOut();
         }
 
-        var dbref = firebase.database().ref().child('clips').child(CurrentAuth.uid);
+        var dbref = firebase.database().ref().child(CurrentAuth.uid);
         $scope.clips = $firebaseArray(dbref);
 
         $scope.toggleSidenav = function () {
